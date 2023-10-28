196a3df1dcad8f26ff41e87414e8be417a3b9ab5
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-28 19:00:53 +0200

    Pol Gubau; MW-Manifest

--- components/meta/meta.tsx
+++ components/meta/meta.tsx
@@ -21,7 +21,7 @@ const Meta = () => {
         sizes="16x16"
         href="/favicon/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="manifest" href="/favicon/manifest.json" />
       <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
